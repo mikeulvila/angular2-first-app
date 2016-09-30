1b078bd7d6ffaa478be1007b35e15e80d04e677e
Mike Ulvila <mike.ulvila@premisehealth.com>
2016-09-30 14:55:20 -0500

    data binding and property binding

---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fa-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent {
+  stringInterpolation = 'This is a string.';
+  numberInterpolation = 2;
+
+}
